9c1eb607ff82523ac4f5591a40d64a5c3deb5d63
Erling Ljunggren <erlingljunggren@gmail.com>
2022-01-31 22:43:59 +0100

    events: disallowing reading event multiple times

--- crates/ly_events/src/lib.rs
+++ crates/ly_events/src/lib.rs
@@ -1,10 +1,9 @@
 use std::marker::PhantomData;
 
-// TODO enable several readers on single channel
-// TODO maybe be inspired by bevy and have two event buffers
 // TODO if two buffers, maybe I can use refcell to enable coupling between
 // readers and channel
 
+#[derive(Debug)]
 enum EventBuffer
 {
 	A,
@@ -15,12 +14,14 @@ struct EventChannel<T>
 {
 	events_a: Vec<T>,
 	events_b: Vec<T>,
-	total_events: usize,
+	start_idx_a: usize,
+	start_idx_b: usize,
 	readable_buffer: EventBuffer,
 }
 
 struct EventReader<T>
 {
+	read_events: usize,
 	dummy_: PhantomData<T>,
 }
 
@@ -31,23 +32,29 @@ impl<T> EventChannel<T>
 		EventChannel {
 			events_a: Vec::with_capacity(10), // maybe sensible initial?
 			events_b: Vec::with_capacity(10), // maybe sensible initial?
-			total_events: 0,
+			start_idx_a: 0,
+			start_idx_b: 0,
 			readable_buffer: EventBuffer::A,
 		}
 	}
 
 	fn send(&mut self, e: T)
 	{
-		self.total_events += 1;
-
 		match self.readable_buffer {
 			EventBuffer::A => {
 				self.events_b.push(e);
+				self.start_idx_b += 1;
 			}
 			EventBuffer::B => {
 				self.events_a.push(e);
+				self.start_idx_a += 1;
 			}
 		}
+		println!(
+			"send: channel start idx a:{} b:{}",
+			self.start_idx_a, self.start_idx_b
+		);
+		println!("send: channel readable_buffer {:?}", self.readable_buffer);
 	}
 
 	fn flush(&mut self)
@@ -56,17 +63,27 @@ impl<T> EventChannel<T>
 			EventBuffer::A => {
 				self.events_a.clear();
 				self.readable_buffer = EventBuffer::B;
+
+				self.start_idx_a = self.start_idx_b // so that reading starts counting properly
 			}
 			EventBuffer::B => {
 				self.events_b.clear();
 				self.readable_buffer = EventBuffer::A;
+
+				self.start_idx_b = self.start_idx_a
 			}
 		}
+		println!(
+			"flush: channel start idx a:{} b:{}",
+			self.start_idx_a, self.start_idx_b
+		);
+		println!("flush: channel readable_buffer {:?}", self.readable_buffer);
 	}
 
 	fn get_reader(&self) -> EventReader<T>
 	{
 		EventReader {
+			read_events: 0,
 			dummy_: Default::default(),
 		}
 	}
@@ -74,13 +91,35 @@ impl<T> EventChannel<T>
 
 impl<T> EventReader<T>
 {
-	fn iter<'a>(&self, channel: &'a EventChannel<T>) -> impl Iterator<Item = &'a T>
+	fn iter<'a>(&mut self, channel: &'a EventChannel<T>) -> impl Iterator<Item = &'a T>
 	{
 		// TODO would like to find a way to couple reader and channel
 		// A naive reference member had lifetime issues with current setup
+		println!("reader read events {}", self.read_events);
+		println!(
+			"channel start idx a:{} b:{}",
+			channel.start_idx_a, channel.start_idx_b
+		);
+		println!("channel readable_buffer {:?}", channel.readable_buffer);
 		match channel.readable_buffer {
-			EventBuffer::A => channel.events_a.iter(),
-			EventBuffer::B => channel.events_b.iter(),
+			EventBuffer::A => {
+				if self.read_events > channel.start_idx_a {
+					[].iter()
+				}
+				else {
+					self.read_events = channel.start_idx_a + 1;
+					channel.events_a.iter()
+				}
+			}
+			EventBuffer::B => {
+				if self.read_events > channel.start_idx_b {
+					[].iter()
+				}
+				else {
+					self.read_events = channel.start_idx_b + 1;
+					channel.events_b.iter()
+				}
+			}
 		}
 	}
 }
@@ -100,23 +139,46 @@ mod tests
 	fn flow()
 	{
 		let mut test_channel = EventChannel::<TestEvent>::new();
-		let event0 = TestEvent { data: 4 };
-		let event1 = TestEvent { data: 2 };
+		let event0 = TestEvent { data: 0 };
+		let event1 = TestEvent { data: 1 };
 
-		let reader = test_channel.get_reader();
+		let mut reader = test_channel.get_reader();
 		let events = reader.iter(&test_channel).collect::<Vec<&TestEvent>>();
-		assert_eq!(events, Vec::<&TestEvent>::new());
+		assert_eq!(events, Vec::<&TestEvent>::new(), "initial events empty");
 
 		test_channel.send(event0);
+		test_channel.flush();
+		let events = reader.iter(&test_channel).collect::<Vec<&TestEvent>>();
+		assert_eq!(
+			events,
+			[&TestEvent { data: 0 }],
+			"reader can read flushed event0"
+		);
+
 		test_channel.send(event1);
 		test_channel.flush();
 
 		let events = reader.iter(&test_channel).collect::<Vec<&TestEvent>>();
-		assert_eq!(events, [&TestEvent { data: 4 }, &TestEvent { data: 2 }]);
-		let reader2 = test_channel.get_reader();
+		assert_eq!(
+			events,
+			[&TestEvent { data: 1 }],
+			"We only retain the events most recently flushed, event0 is then dropped"
+		);
 
+		let mut reader2 = test_channel.get_reader();
 		let events = reader2.iter(&test_channel).collect::<Vec<&TestEvent>>();
-		assert_eq!(events, [&TestEvent { data: 4 }, &TestEvent { data: 2 }]);
+		assert_eq!(
+			events,
+			[&TestEvent { data: 1 }],
+			"We only retain the events most recently flushed, reader2 reads after event0 has been \
+			 dropped"
+		);
+		let events = reader2.iter(&test_channel).collect::<Vec<&TestEvent>>();
+		assert_eq!(
+			events,
+			Vec::<&TestEvent>::new(),
+			"Cannot read event multiple times"
+		);
 
 		test_channel.flush();
 		let events = reader2.iter(&test_channel).collect::<Vec<&TestEvent>>();
