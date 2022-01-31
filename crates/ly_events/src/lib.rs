68a7f19550b4d2692fd2aaf7edce0eb7248c6746
Erling Ljunggren <erlingljunggren@gmail.com>
2022-01-31 21:54:04 +0100

    events: two event buffers

--- crates/ly_events/src/lib.rs
+++ crates/ly_events/src/lib.rs
@@ -5,9 +5,18 @@ use std::marker::PhantomData;
 // TODO if two buffers, maybe I can use refcell to enable coupling between
 // readers and channel
 
+enum EventBuffer
+{
+	A,
+	B,
+}
+
 struct EventChannel<T>
 {
-	events: Vec<T>,
+	events_a: Vec<T>,
+	events_b: Vec<T>,
+	total_events: usize,
+	readable_buffer: EventBuffer,
 }
 
 struct EventReader<T>
@@ -17,11 +26,43 @@ struct EventReader<T>
 
 impl<T> EventChannel<T>
 {
-	fn new() -> EventChannel<T> { EventChannel { events: Vec::new() } }
+	fn new() -> EventChannel<T>
+	{
+		EventChannel {
+			events_a: Vec::with_capacity(10), // maybe sensible initial?
+			events_b: Vec::with_capacity(10), // maybe sensible initial?
+			total_events: 0,
+			readable_buffer: EventBuffer::A,
+		}
+	}
 
-	fn send(&mut self, e: T) { self.events.push(e); }
+	fn send(&mut self, e: T)
+	{
+		self.total_events += 1;
 
-	fn flush(&mut self) { self.events.clear(); }
+		match self.readable_buffer {
+			EventBuffer::A => {
+				self.events_b.push(e);
+			}
+			EventBuffer::B => {
+				self.events_a.push(e);
+			}
+		}
+	}
+
+	fn flush(&mut self)
+	{
+		match self.readable_buffer {
+			EventBuffer::A => {
+				self.events_a.clear();
+				self.readable_buffer = EventBuffer::B;
+			}
+			EventBuffer::B => {
+				self.events_b.clear();
+				self.readable_buffer = EventBuffer::A;
+			}
+		}
+	}
 
 	fn get_reader(&self) -> EventReader<T>
 	{
@@ -37,7 +78,10 @@ impl<T> EventReader<T>
 	{
 		// TODO would like to find a way to couple reader and channel
 		// A naive reference member had lifetime issues with current setup
-		channel.events.iter()
+		match channel.readable_buffer {
+			EventBuffer::A => channel.events_a.iter(),
+			EventBuffer::B => channel.events_b.iter(),
+		}
 	}
 }
 
@@ -63,6 +107,7 @@ mod tests
 		assert_eq!(events, Vec::<&TestEvent>::new());
 		test_channel.send(event0);
 		test_channel.send(event1);
+		test_channel.flush();
 		let events = reader.iter(&test_channel).collect::<Vec<&TestEvent>>();
 		assert_eq!(events, [&TestEvent { data: 4 }, &TestEvent { data: 2 }]);
 	}
