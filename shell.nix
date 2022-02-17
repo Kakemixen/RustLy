6196e6a0a9cdb762f553c6173df49b6ea74952cd
Erling Ljunggren <erlingljunggren@gmail.com>
2022-02-17 22:52:15 +0100

    fix rust-overlay tag

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 let
 	nixgl   = import (fetchTarball "https://github.com/guibou/nixGL/archive/804f1989b3f0bb3347c02ce136060e29f9fc3340.tar.gz") {};
-	rustOverlay = import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+	rustOverlay = import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/d480bb17451c57cf4ef67a14f6772f752ced382c.tar.gz");
 	pkgs    = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/93ca5ab64f78ce778c0bcecf9458263f0f6289b6.tar.gz") {
 		overlays = [ rustOverlay ];
 	};
